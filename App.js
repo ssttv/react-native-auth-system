dbf21f027344f1281daff9a20f54d6376d16379b
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-07-31 23:33:02 +0300

    chore: add platform-dependant padding

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: 'center',
+    justifyContent: 'center',
+    ...Platform.select({
+      ios: {
+        paddingTop: 30
+      },
+      android: {
+        paddingTop: 20
+      }
+    })
+  }
+});
+
 export default class App extends React.Component {
   render() {
     return (
@@ -10,12 +27,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
