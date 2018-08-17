cc1bfa786ea822df3c974fa78a54c881fc6b1fd1
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-08-17 22:10:13 +0300

    chore: kick out unused deps to comply with linters

--- components/SignupForm.js
+++ components/SignupForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { FormLabel, FormInput, Button } from "react-native-elements";
 
 class SignupForm extends Component {
